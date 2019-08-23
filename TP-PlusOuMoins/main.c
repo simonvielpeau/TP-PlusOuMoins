089c530b7ef7f2914338e2972102e817828b3025
Simon Vielpeau <simon@vielpeau.com>
2019-08-23 21:38:27 +0200

    my version is finished, and it works

--- TP-PlusOuMoins/main.c
+++ TP-PlusOuMoins/main.c
@@ -12,6 +12,29 @@
 
 int main(int argc, char *argv[]) // Équivalent de int main()
 {
+    srand(time(NULL));
+    
+    const int MAX = 100, MIN = 1;
+    int nombreMystere = 0;
+    
+    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+    
+    int nombreEntree = 0;
+    
+    while(nombreMystere != nombreEntree){
+        printf("Quel est le nombre ? ");
+        scanf("%d", &nombreEntree);
+        
+        if(nombreMystere > nombreEntree) {
+            printf("C'est plus !\n\n");
+        }
+        else if (nombreMystere < nombreEntree) {
+            printf("C'est moins !\n\n");
+        }
+    }
+    
+    printf("Bravo, vous avez trouve le nombre mystère !!!\n\n");
+    
     
     return 0;
 }
