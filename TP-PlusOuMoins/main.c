a31159f4483ee5f43cde1553c244c1d8e04d4a25
Simon Vielpeau <simon@vielpeau.com>
2019-08-23 10:48:27 +0200

    Initial Commit

---
+++ TP-PlusOuMoins/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TP-PlusOuMoins
+//
+//  Created by Simon Vielpeau on 23/08/2019.
+//  Copyright © 2019 Simon Vielpeau. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
