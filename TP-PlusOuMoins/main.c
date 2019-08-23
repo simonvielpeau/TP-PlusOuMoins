4f691eb021d4e1fb82f1d5d458284df0a3612c06
Simon Vielpeau <simon@vielpeau.com>
2019-08-23 22:03:30 +0200

    add a question which asks if he wants to play again

--- TP-PlusOuMoins/main.c
+++ TP-PlusOuMoins/main.c
@@ -14,28 +14,51 @@ int main(int argc, char *argv[]) // Équivalent de int main()
 {
     srand(time(NULL));
     
-    const int MAX = 100, MIN = 1;
-    int nombreMystere = 0;
+    int continuerPartie = 1;
     
-    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
-    
-    int nombreEntre = 0;
-    int coups = 0;
-    
-    for(coups = 0; nombreMystere != nombreEntre; coups++){
-        printf("Quel est le nombre ? ");
-        scanf("%d", &nombreEntre);
+    while (continuerPartie == 1) {
+        const int MAX = 100, MIN = 1;
+        int nombreMystere = 0;
         
-        if(nombreMystere > nombreEntre) {
-            printf("C'est plus !\n\n");
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+        
+        int nombreEntre = 0;
+        int coups = 0;
+        
+        for(coups = 0; nombreMystere != nombreEntre; coups++){
+            printf("Quel est le nombre ? ");
+            scanf("%d", &nombreEntre);
+            
+            if(nombreMystere > nombreEntre) {
+                printf("C'est plus !\n\n");
+            }
+            else if (nombreMystere < nombreEntre) {
+                printf("C'est moins !\n\n");
+            }
         }
-        else if (nombreMystere < nombreEntre) {
-            printf("C'est moins !\n\n");
-        }
+        
+        printf("\nFélicitations ! Vous avez trouvé le nombre mystère en %d coups !\n\n", coups);
+        
+        do {
+            printf("Voulez-vous faire une nouvelle partie ?\n");
+            printf("Veuillez répondre par 1 (oui), ou par 2 (non)\n");
+            scanf("%d", &continuerPartie);
+            
+            switch (continuerPartie) {
+                case 1:
+                    printf("\nC'est parti pour une nouvelle tentative !\n\n");
+                    break;
+                case 2:
+                    printf("\nD'accord, arrêtons-nous là !\n\n");
+                    break;
+                default:
+                    printf("Vous devez soit répondre par 1 (oui), ou par 2 (non)");
+                    break;
+            }
+        } while (continuerPartie != 1 && continuerPartie != 2);
     }
     
-    printf("\n\nFélicitations ! Vous avez trouvé le nombre mystère en %d coups !\n\n", coups);
-    
+   
     
     return 0;
 }
